bd958640e3ae949c88a96937416f2b29a1d08857
Al Rubin <agrubin@hotmail.com>
2016-02-24 02:44:00 -0500

    Inital commit.

---
+++ ngex/LexXML.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ngex
+{
+    public static class Transform
+    {
+        private static readonly List<char> metacharacters = new List<char>() { '\\', '^', '$', '.', '|', '?', '*', '+', '(', ')', '[', '{' }; 
+        
+        public static string MetaCharacter(char mchar)
+        {
+            if (metacharacters.Contains(mchar))
+            {
+                return "\\" + mchar;
+            }
+            else
+            {
+                return mchar.ToString();
+            }
+        }
+    }
+    public class LexXML
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LexXML"/> class.
+        /// </summary>
+        public LexXML()
+        {
+            
+        }
+    }
+}
+ 
---
+++ ngexTester/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ngex;
+
+
+namespace ngexTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LexXML lX = new LexXML();
+        }
+    }
+}
