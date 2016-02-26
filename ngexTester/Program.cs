c5efb380b8447238779c9d4a8c382b8e73d0b38b
Al Rubin <agrubin@hotmail.com>
2016-02-26 04:25:42 -0500

    sync up

--- ngexTester/Program.cs
+++ ngexTester/Program.cs
@@ -1,22 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using ngex;
+
 
 namespace ngexTester
 {
     class Program
     {
-        static void MainLogHandler(object sender, LogEventArgs le)
-        {
-            Console.WriteLine(le.timestamp + " - " + le.description);
-        } 
+    
         static void Main(string[] args)
         {
-
-            Error.logevent += MainLogHandler;
-            new Error(null, "test");
-            int g = 7;
+   
+      
         }
     }
 }
