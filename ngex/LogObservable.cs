41938283020ce10139c5db85da6466d8db4b4af8
Al Rubin <agrubin@hotmail.com>
2016-03-01 23:40:38 -0500

    Trying to work out PCL reflection of method names...

--- ngex/LogObservable.cs
+++ ngex/LogObservable.cs
@@ -1,15 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace ngex
 {
     public class LogObservable
     {
+        /// <summary>
+        /// Subscribe to logevent for logging.
+        /// </summary>
         // Fields...
-        public static event LogEventHandler logevent;
+         static event LogEventHandler logevent;
 
         protected void Post(object observedtype, Exception caughtexception, string message)
         {
+            StackTrace st = new StackTrace();
+            StackFrame sf = st.GetFrame(1);
+
+            return sf.GetMethod().Name;
             logevent?.Invoke(observedtype, new LogEventArgs(message, caughtexception));
         }
     }
--- ngexTester/Program.cs
+++ ngexTester/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Diagnostics;
+using System.Text;
 using ngex;
 
 
@@ -11,21 +12,25 @@ namespace ngexTester
     
     class Program
     {
-        //static void Main(string[] args)
-        //{
-        //    LogObservable.logevent += LogHandler;
-        //    TextWriterTraceListener myTextListener = new TextWriterTraceListener(System.Console.Out);
-        //    Trace.Listeners.Add(myTextListener);
+        static void Main(string[] args)
+        {
+            LogObservable.logevent += LogHandler;
+            TextWriterTraceListener myTextListener = new TextWriterTraceListener(System.Console.Out);
+            Trace.Listeners.Add(myTextListener);
 
-        //    XMLConverter converter = new XMLConverter();
-        //    converter.FromString(@"<hi>there</hi>");           
-        //}
+            XMLConverter converter = new XMLConverter();
+            converter.FromString(@"<hithere</hi>");           
+        }
 
-        //static void LogHandler(object sender, LogEventArgs logeventargs)
-        //{
-        //    Trace.Write("Test output ");
-        //    Trace.Flush();
-        //    int j = 7;
-        //}
+        static void LogHandler(object sender, LogEventArgs logeventargs)
+        {
+            StringBuilder logentry = new StringBuilder();
+            logentry.Append(logeventargs.timestamp).AppendLine();
+            logentry.Append(logeventargs.description).AppendLine();
+            logentry.Append(logeventargs?.exception.Message).AppendLine();
+            logentry.Append("-------------------").AppendLine();
+            Trace.Write(logentry);
+            Trace.Flush();
+        }
     }
 }
